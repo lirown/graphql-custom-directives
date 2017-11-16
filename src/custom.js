25693136aa3a42651fe9a2b938510ebd3d8562e9
assafmilman <assaf.milman@forter.com>
2017-11-16 17:21:55 +0200

    added mutation query type for reoslve wrapping

--- src/custom.js
+++ src/custom.js
@@ -136,6 +136,7 @@ exports.applySchemaCustomDirectives = function (schema) {
   }
 
   wrapFieldsWithMiddleware(schema._queryType);
+  wrapFieldsWithMiddleware(schema._mutationType);
 
-  return true;
+    return true;
 };
