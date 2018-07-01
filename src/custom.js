6a44510ab1de3cbb79a36e09f459dba61630ada4
Lirown <lgoldenberg@forter.com>
2018-07-01 12:52:48 +0300

    reindent

--- src/custom.js
+++ src/custom.js
@@ -142,7 +142,7 @@ function resolveMiddlewareWrapper(resolve = defaultResolveFn, directives = {}) {
  * of the graphql custom directives resolve execution
  */
 function wrapFieldsWithMiddleware(type, deepWrap = true, typeMet = {}) {
-  if(!type){
+  if (!type) {
     return;
   }
 
@@ -152,9 +152,12 @@ function wrapFieldsWithMiddleware(type, deepWrap = true, typeMet = {}) {
     let field = fields[label];
     if (field && !typeMet[field.type.name]) {
       if (!!field && typeof field == 'object') {
-        field.resolve = resolveMiddlewareWrapper(field.resolve, field.directives);
+        field.resolve = resolveMiddlewareWrapper(
+          field.resolve,
+          field.directives,
+        );
         if (field.type._fields && deepWrap) {
-          wrapFieldsWithMiddleware(field.type, deepWrap, typeMet)
+          wrapFieldsWithMiddleware(field.type, deepWrap, typeMet);
         } else if (field.type.ofType && field.type.ofType._fields && deepWrap) {
           wrapFieldsWithMiddleware(field.type.ofType, deepWrap, typeMet);
         }
